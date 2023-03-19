1228feeb0f7502f86b79b969bdee121cca1c8f6f
Vlad <v.karmazin@bk.ru>
2023-03-19 12:37:08 +0400

    Vlad

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,28 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    int positionNow = 0;
+    final int [] array = DecrementingCarousel.carousel.clone();
+
 
     public int next() {
-       throw new UnsupportedOperationException();
+        if (isFinished()) {
+            return -1;
+        } else {
+            while (array[positionNow %= array.length] <= 0) {       //Честно позаимствованная проверка
+                positionNow++;
+            }
+        }
+        int returnValue = array [positionNow];
+        array[positionNow]--;
+        positionNow++;
+        return returnValue;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        for (int elementsArr:array)
+            if (elementsArr > 0) return false;
+        return true;
     }
 
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,28 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+    static int [] carousel;
+    private final int capacity;
+    int index = 0;
+    boolean isRun = false;
 
+    public DecrementingCarousel(int capacity) {
+        this.capacity = capacity;
+        carousel = new int[capacity];
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (element > 0 && index < capacity && !isRun){
+            carousel [index] = element;
+            index++;
+            return true;
+        } return false;
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        if (!isRun) {
+            isRun = true;
+            return new CarouselRun();
+        } return null;
     }
 }
